ef3b52c59162a86a9590b2e242050cea995272d3
Jina <jinatye@naver.com>
2026-04-21 01:37:39 +0900

    feat: 필터링 기능 구현(기본)

--- Week2/Homework/money-log/index.js
+++ Week2/Homework/money-log/index.js
@@ -1,37 +1,101 @@
 import { expenses } from "./mockData.js";
 
-const expenseList = document.getElementById("expense-list");
-
-const storedExpenses = JSON.parse(localStorage.getItem("expenseData"));
-
+/** 셀 만들기 함수 */
 const createCell = (text) => {
   const td = document.createElement("td");
   td.textContent = text;
   return td;
 };
 
-storedExpenses.forEach((expense) => {
-  const tr = document.createElement("tr");
+/** 표 렌더링 함수 */
+const expenseList = document.querySelector("#expense-list");
 
-  const checkTd = document.createElement("td");
-  const checkbox = document.createElement("input");
-  checkbox.type = "checkbox";
+const renderExpenses = (expenseArray) => {
+  expenseList.innerHTML = "";
 
-  checkTd.appendChild(checkbox);
-  tr.appendChild(checkTd);
-  tr.dataset.id = expense.id;
+  expenseArray.forEach((expense) => {
+    const tr = document.createElement("tr");
 
-  const titleTd = createCell(expense.title);
-  const amountTd = createCell(expense.amount);
-  const dateTd = createCell(expense.date);
-  const categoryTd = createCell(expense.category);
-  const paymentTd = createCell(expense.payment);
+    const checkTd = document.createElement("td");
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
 
-  tr.appendChild(titleTd);
-  tr.appendChild(amountTd);
-  tr.appendChild(dateTd);
-  tr.appendChild(categoryTd);
-  tr.appendChild(paymentTd);
+    checkTd.appendChild(checkbox);
+    tr.appendChild(checkTd);
+    tr.dataset.id = expense.id;
 
-  expenseList.appendChild(tr);
-});
+    const titleTd = createCell(expense.title);
+    const amountTd = createCell(expense.amount);
+    const dateTd = createCell(expense.date);
+    const categoryTd = createCell(expense.category);
+    const paymentTd = createCell(expense.payment);
+
+    tr.appendChild(titleTd);
+    tr.appendChild(amountTd);
+    tr.appendChild(dateTd);
+    tr.appendChild(categoryTd);
+    tr.appendChild(paymentTd);
+
+    expenseList.appendChild(tr);
+  });
+};
+
+// localStorage의 데이터를 테이블에 렌더링
+const storedExpenses = JSON.parse(localStorage.getItem("expenseData")) || [];
+
+renderExpenses(storedExpenses);
+
+// 검색 필터링
+const titleInput = document.querySelector("#title");
+const typeInput = document.querySelector("#type");
+const categoryInput = document.querySelector("#category");
+const paymentInput = document.querySelector("#payment");
+
+const applyButton = document.querySelector("#filter-apply-button");
+const resetButton = document.querySelector("#filter-reset-button");
+
+/** 필터링 적용 버튼 (개별로만 가능) */
+const applyFiltering = () => {
+  const keyword = titleInput.value.trim();
+  const type = typeInput.value;
+  const category = categoryInput.value;
+  const payment = paymentInput.value;
+
+  let filteringExpenses = storedExpenses;
+
+  if (keyword !== "") {
+    filteringExpenses = storedExpenses.filter((expense) =>
+      expense.title.includes(keyword),
+    );
+  } else if (type !== "") {
+    filteringExpenses =
+      type === "수입"
+        ? storedExpenses.filter((expense) => expense.amount > 0)
+        : storedExpenses.filter((expense) => expense.amount < 0);
+  } else if (category !== "") {
+    filteringExpenses = storedExpenses.filter(
+      (expense) => expense.category === category,
+    );
+  } else if (payment !== "") {
+    filteringExpenses = storedExpenses.filter(
+      (expense) => expense.payment === payment,
+    );
+  }
+
+  console.log(filteringExpenses);
+
+  renderExpenses(filteringExpenses);
+};
+
+/** 초기화 버튼 */
+const resetFiltering = () => {
+  titleInput.value = "";
+  typeInput.value = "";
+  categoryInput.value = "";
+  paymentInput.value = "";
+
+  renderExpenses(storedExpenses);
+};
+
+applyButton.addEventListener("click", applyFiltering);
+resetButton.addEventListener("click", resetFiltering);
